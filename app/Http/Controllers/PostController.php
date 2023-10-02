d0c309a8eb70ccbb2455ceb184ba8a7a68198171
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2023-10-02 22:13:55 +0700

    halaman tambah data Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 
 use Illuminate\View\View;
 
+use Illuminate\Http\RedirectResponse;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -15,5 +17,30 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
 
         return view('posts.index', compact('posts'));
+    }
+
+    public function create() : View
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) : RedirectResponse
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil disimpan!']);
     }
 }
